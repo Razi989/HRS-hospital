790833c685239ed4885e51b51718880f38135113
Razi989 <107915843+Razi989@users.noreply.github.com>
2026-04-06 16:49:30 +0530

    stable deploy

--- Server.js
+++ Server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-// TEST ROUTE
+// static frontend
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("LIVE 🚀");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// PORT (Railway compatible)
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server started " + PORT);
+  console.log("App Live 🚀");
 });
