58a0ce6d2cfa13cecf148031ad22849b75feec7c
Razi989 <107915843+Razi989@users.noreply.github.com>
2026-04-06 11:00:45 +0530

    fix crash

--- Server.js
+++ Server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 
-// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Server chal raha hai 🚀");
 });
 
-// SAFE PORT
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
